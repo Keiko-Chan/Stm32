601701c3af8e293d0a1018d996b0e58e97ba5e68
Ekaterina <70318220+Keiko-Chan@users.noreply.github.com>
2022-04-03 18:33:08 +0300

    Add files via upload

--- one_butt.c
+++ one_butt.c
@@ -53,7 +53,6 @@ static void rcc_config()
 }
 static int indicator2 = 0;
 static int milliseconds = 0;
-static int ms_old = 0;
 static int statement = 0;
 static uint32_t note1[] = {1, 341, 286, 227, 191};
 
@@ -104,9 +103,8 @@ static void timers_config(void)
 
 static void exti_config()
 {
-    LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_SYSCFG);
-    LL_GPIO_SetPinPull(GPIOB, LL_GPIO_PIN_0, LL_GPIO_PULL_UP);
-    
+	LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_SYSCFG);
+	LL_GPIO_SetPinPull(GPIOB, LL_GPIO_PIN_0, LL_GPIO_PULL_UP);
     LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTB, LL_SYSCFG_EXTI_LINE0);
     
     LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_0);
@@ -116,47 +114,72 @@ static void exti_config()
     NVIC_SetPriority(EXTI0_1_IRQn, 0);
 }
 
-void EXTI0_1_IRQHandler()
-{    
-    int ms = milliseconds;
+struct butt
+{
+	int num;				//number of button
+	uint32_t el;	        //exti line
+	int statement;			//pressed or not
+	int ms_old;				//time beetween pressed (to eliminate inaccuracy)	
+};
+
+struct sw
+{
+	int indicator;
+	int t_busy;
+};
+
+static struct butt b0 = {0, LL_EXTI_LINE_0, 0, 0};
+static struct sw s1 = {0, 0};
+
+int chose_sw (int num_but)
+{
+	s1.indicator = num_but + 1;
+	s1.t_busy = milliseconds;
+	return 0; 
+}
+
+int butt_handler(struct butt * but)
+{
+	int ms = milliseconds;
      
-    //change indicator2 with buttons B0 and B1
-    if(ms - ms_old > 90)
+    if(ms - but->ms_old > 90)
     {    
         LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_8);
-        ms_old = milliseconds;  
+        but->ms_old = milliseconds;  
         
-        
-        if(LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_0))
-        {
-                    
-              if(statement == 0) 
-              {
-                  indicator2 = 1;
-                  statement = 1;
-                  LL_EXTI_EnableFallingTrig_0_31(LL_EXTI_LINE_0);                  
-              }
+        if(LL_EXTI_IsActiveFlag_0_31(but->el))
+        {                    
+         	if(statement == 0) 
+            {
+                but->statement = 1;
+                LL_EXTI_EnableFallingTrig_0_31(but->el);                  
+            }
               
-              else 
-              {
-                  if(indicator2 == 1)
-                      indicator2 = 0;
-                  statement = 0;
-                  LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_0);                  
-              }
-        }      
-    }
+            else 
+            {
+            	but->statement = 0;
+                LL_EXTI_EnableRisingTrig_0_31(but->el);                  
+            }
+		}      
+   	}
    
-    if(ms - ms_old <= 90 && ms - ms_old > 0 && statement == 1 && LL_EXTI_IsActiveFlag_0_31(LL_EXTI_LINE_0))
+    if(ms - but->ms_old <= 90 && ms - but->ms_old > 0 && but->statement == 1 && LL_EXTI_IsActiveFlag_0_31(but->el))
     {    
-          ms_old = milliseconds; 
-          if(indicator2 == 1)
-              indicator2 = 0;
-          statement = 0;
-          LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_0);         
+    	but->ms_old = milliseconds; 
+        but->statement = 0;
+        LL_EXTI_EnableRisingTrig_0_31(but->el);         
     }
+
+	if(but->statement == 1)
+		chose_sw (but->num);
     
-    LL_EXTI_ClearFlag_0_31(LL_EXTI_LINE_0);
+   	LL_EXTI_ClearFlag_0_31(but->el);
+    return 0;
+}
+
+void EXTI0_1_IRQHandler()
+{    
+	butt_handler(&b0);
 }
 
 void SysTick_Handler(void)
@@ -166,13 +189,13 @@ void SysTick_Handler(void)
    
     if(LL_GPIO_IsInputPinSet(GPIOB, LL_GPIO_PIN_0) == 1)
     {
-        indicator2 = 0;
-        statement = 0;
-        LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_0); 
+        s1.indicator = 0;
+        b0.statement = 0;
+        LL_EXTI_EnableRisingTrig_0_31(b0.el); 
     }      
-    
-    LL_TIM_OC_SetCompareCH1(TIM2, (uint32_t)(0.96 * (note1[indicator2])));
-        LL_TIM_SetAutoReload(TIM2, note1[indicator2]);
+
+    LL_TIM_OC_SetCompareCH1(TIM2, (uint32_t)(0.96 * (note1[s1.indicator])));
+        LL_TIM_SetAutoReload(TIM2, note1[s1.indicator]);
 }
 
 int main(void)
@@ -190,6 +213,5 @@ int main(void)
         
     }
 
-    while (1);
     return 0;
 }
